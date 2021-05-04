3b3c53e362e60c84e1740e4bebaedfaf39bf5663
Shashi Shekhar <shashi20008@gmail.com>
2021-05-04 21:08:21 +0530

    Lock scrolling when greeting is displayed

--- src/components/lib/FullPageCover.tsx
+++ src/components/lib/FullPageCover.tsx
@@ -1,8 +1,15 @@
-import { ReactElement } from 'react';
+import { ReactElement, useEffect } from 'react';
 
 import './FullPageCover.css';
 
 function FullPageCover({ center, children, className }: FullPageCoverProps): ReactElement {
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+
+    return () => {
+      document.body.style.overflow = 'visible';
+    };
+  }, []);
   return <div className={`fullpage-cover ${className} ${center ? 'center' : ''}`}>{children}</div>;
 }
 
